5edff03f362df3b7f4095a4972fb67aac5d85d48
Nikita Bozhenkov <nikbozhenkov@gmail.com>
2019-10-24 08:59:01 +0300

    Converter fixed

--- Swing-Lab-Work/src/converter/Converter.java
+++ Swing-Lab-Work/src/converter/Converter.java
@@ -1,7 +1,6 @@
 package converter;
 
 import bag.Bag;
-import exceptions.VolumeException;
 import geometricFigures.GeometricFigure;
 import shapes.*;
 
@@ -10,24 +9,28 @@ import java.util.Random;
 public class Converter {
     public <U extends GeometricFigure> Shape convert(U figure, Bag bag) {
         Random random = new Random();
-        if (figure.getSquare() > bag.getVolume() * 100) throw new VolumeException("Too big figure to create");
-        else if (figure.getSquare() < 0.01) throw new VolumeException("Too small figure to create");
-        double height = Math.round((Math.random()*(bag.getVolume()/figure.getSquare()+0.999) + 0.001) * 100) /100;
+        if (figure.getArea() > bag.getVolume() * 100) {
+            throw new RuntimeException("Too big figure to create");
+        }
+        else if (figure.getArea() < 0.01) {
+            throw new RuntimeException("Too small figure to create");
+        }
+        double height = Math.round((Math.random()*(bag.getVolume()/figure.getArea()+0.999) + 0.001) * 100) /100;
         switch (figure.toString()) {
             case "Circle" : {
                 if (random.ints(0,5).findFirst().getAsInt() < 2) {
-                    return new Sphere(figure.getRadius());
+                    return new Sphere(figure.getFirstField());
                 } else {
-                    return new Cylinder(figure.getRadius(), height);
+                    return new Cylinder(figure.getFirstField(), height);
                 }
             }
             case "Triangle" : {
-                return new Pyramid(figure.getSide(), figure.getHeight(), height);
+                return new Pyramid(figure.getFirstField(), figure.getSecondField(), height);
             }
             case "Square" : {
-                return new Cube(figure.getSide());
+                return new Cube(figure.getFirstField());
             }
-            default: return new Sphere(0);
+            default: throw new RuntimeException("Can't convert this figure");
         }
     }
 }
