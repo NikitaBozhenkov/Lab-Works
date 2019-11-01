c2aec83f8e86be5e679ce1cc7d220c7c97efa18c
Nikita Bozhenkov <nikbozhenkov@gmail.com>
2019-11-01 13:57:30 +0300

    created package

---
+++ Docks/src/main/Main.java
@@ -0,0 +1,11 @@
+package main;
+
+import sandwich.SandwichIngredient;
+import ships.Ship;
+
+public class Main {
+    public static void main(String[] args) {
+        Ship breadShip = new Ship(SandwichIngredient.BREAD, 10);
+
+    }
+}
