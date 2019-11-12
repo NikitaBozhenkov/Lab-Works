bed018590f02bd702e09f60eab533e7723fa76fd
Nikita Bozhenkov <nikbozhenkov@gmail.com>
2019-11-12 23:38:07 +0300

    Simple ship created

---
+++ Docks/src/ships/Ship.java
@@ -0,0 +1,21 @@
+package ships;
+
+import sandwich.Ingredient;
+
+public class Ship {
+    private int cargoWeight;
+    private Ingredient cargoType;
+
+    public Ship(int cargoWeight, Ingredient cargoType) {
+        this.cargoWeight = cargoWeight;
+        this.cargoType = cargoType;
+    }
+
+    public Ingredient getCargoType() {
+        return cargoType;
+    }
+
+    public int getCargoWeight() {
+        return cargoWeight;
+    }
+}
