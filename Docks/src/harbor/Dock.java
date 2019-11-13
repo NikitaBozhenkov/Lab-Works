b34492873b1f2ae4cf45f99932d91a3856685ddf
Nikita Bozhenkov <nikbozhenkov@gmail.com>
2019-11-13 23:38:12 +0300

    Simple dock created

--- Docks/src/harbor/Dock.java
+++ Docks/src/harbor/Dock.java
@@ -1,5 +1,31 @@
 package harbor;
 
+import narrowTunnel.NarrowTunnel;
+import sandwich.Ingredient;
+import ships.Ship;
+
 public class Dock extends Thread {
     private Stock stock;
+    private Ingredient ingredient;
+    private Ship ship;
+    private NarrowTunnel tunnel;
+    private boolean isFree;
+
+    public void shipCall(NarrowTunnel tunnel) {
+
+    }
+
+    public Ingredient getIngredient() {
+        return ingredient;
+    }
+
+    public boolean isFree() {
+        return isFree;
+    }
+
+    private void updateStatus() {
+        if (ship != null) {
+            isFree = false;
+        }
+    }
 }
