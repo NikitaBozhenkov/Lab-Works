c0d5a2bec5d56f6296b9a81c3b17adac66aca332
Nikita Bozhenkov <nikbozhenkov@gmail.com>
2019-11-14 20:39:45 +0300

    Added dock.run() realization

--- Docks/src/harbor/Dock.java
+++ Docks/src/harbor/Dock.java
@@ -5,14 +5,57 @@ import sandwich.Ingredient;
 import ships.Ship;
 
 public class Dock extends Thread {
-    private Stock stock;
-    private Ingredient ingredient;
+    private volatile Stock stock;
+    private final Ingredient ingredient;
     private Ship ship;
-    private NarrowTunnel tunnel;
-    private boolean isFree;
+    private volatile NarrowTunnel tunnel;
+    private volatile boolean isFree;
 
-    public void shipCall(NarrowTunnel tunnel) {
+    public Dock(Ingredient ingredient, Stock stock) {
+        this.ingredient = ingredient;
+        this.stock = stock;
+    }
 
+    @Override
+    public void run() {
+        while(true) {
+            //trying to get a ship
+            shipCall();
+            if (ship != null) {
+                // Operated check
+                while(stock.isOperated()) {
+                    try {
+                        wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                stock.setOperatedFlag(true);
+
+                //Unloading the cargo
+                while (ship.getCargoWeight() != 0) {
+                    try {
+                        ship.cargoUnload();
+                        stock.operateGoods(true);
+                        sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                stock.setOperatedFlag(false);
+            }
+
+            //Sleep after the whole cycle or bad call
+            try {
+                sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void shipCall() {
+        ship = tunnel.getShipOut(ingredient);
     }
 
     public Ingredient getIngredient() {
