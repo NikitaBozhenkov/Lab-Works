b8d474318a3f4a850d559ba679ed17a4e9b6bc60
Nikita Bozhenkov <nikbozhenkov@gmail.com>
2019-11-16 18:21:10 +0300

    Main changed for testing

--- Docks/src/main/Main.java
+++ Docks/src/main/Main.java
@@ -3,8 +3,11 @@ package main;
 import harbor.Dock;
 import harbor.Harbor;
 import harbor.Stock;
+import hobos.Hobo;
+import hobos.HobosHovel;
 import narrowTunnel.NarrowTunnel;
 import sandwich.Ingredient;
+import sandwich.IngredientsHeap;
 import shipsGenerator.ShipsGenerator;
 
 public class Main {
@@ -14,6 +17,21 @@ public class Main {
         Harbor harbor = new Harbor(new Dock(Ingredient.BREAD, new Stock(), tunnel),
                 new Dock(Ingredient.MAYONNAISE, new Stock(), tunnel),
                 new Dock(Ingredient.SAUSAGE, new Stock(), tunnel));
+        HobosHovel hobosHovel = new HobosHovel(new IngredientsHeap());
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        Hobo[] hobos = new Hobo[]{new Hobo(harbor, "#1", hobosHovel),
+                new Hobo(harbor, "#2", hobosHovel), new Hobo(harbor, "#3", hobosHovel),
+                new Hobo(harbor, "#4", hobosHovel), new Hobo(harbor, "#5", hobosHovel),
+                new Hobo(harbor, "#6", hobosHovel), new Hobo(harbor, "#7", hobosHovel),
+                new Hobo(harbor, "#8", hobosHovel)};
+
+        for(Hobo hobo : hobos) {
+            hobo.start();
+        }
         shipsGenerator.start();
         harbor.startWorkingDay();
     }
